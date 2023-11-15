6192d2fcb7d125feea76441679cf517961ee85be
andy <91167584+andymutale@users.noreply.github.com>
2023-11-15 04:41:50 +0200

    Update functions.c
    g

--- functions.c
+++ functions.c
@@ -11,7 +11,112 @@ int print_string(va_list types, char buffer[], int flags, int width, int precisi
 	int length = 0, i;
 	char *str = va_arg(types, char *);
 
-	if (str == NULL)
+	if (str =#include "main.h"
+
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+    char c = va_arg(types, int);
+    return (handle_write_char(c, buffer, flags, width, precision, size));
+}
+
+int print_string(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+    int length = 0, i;
+    char *str = va_arg(types, char *);
+
+    if (str == NULL)
+        str = "(null)";
+
+    while (str[length] != '\0')
+        length++;
+
+    if (precision >= 0 && precision < length)
+        length = precision;
+
+    if (width > length)
+    {
+        if (flags & F_MINUS)
+        {
+            write(1, &str[0], length);
+            for (i = width - length; i > 0; i--)
+                write(1, " ", 1);
+            return (width);
+        }
+        else
+        {
+            for (i = width - length; i > 0; i--)
+                write(1, " ", 1);
+            write(1, &str[0], length);
+            return (width);
+        }
+    }
+
+    return (write(1, str, length));
+}
+
+int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+    return (write(1, "%%", 1));
+}
+
+int print_int(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+    int i = BUFF_SIZE - 2;
+    int is_negative = 0;
+    long int n = va_arg(types, long int);
+    unsigned long int num;
+
+    n = convert_size_number(n, size);
+
+    if (n == 0)
+        buffer[i--] = '0';
+
+    buffer[BUFF_SIZE - 1] = '\0';
+    num = (unsigned long int)n;
+
+    if (n < 0)
+    {
+        num = (unsigned long int)((-1) * n);
+        is_negative = 1;
+    }
+
+    while (num > 0)
+    {
+        buffer[i--] = (num % 10) + '0';
+        num /= 10;
+    }
+
+    i++;
+
+    // Null-terminate the buffer
+    buffer[i + 1] = '\0';
+
+    return (write_number(is_negative, i, buffer, flags, width, precision, size));
+}
+
+int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+    unsigned int n = va_arg(types, unsigned int);
+    char binary[32];
+    int count = 0;
+
+    for (int i = 0; i < 32; i++)
+    {
+        binary[i] = (n & (1 << (31 - i))) ? '1' : '0';
+    }
+
+    for (int i = 0; i < 32; i++)
+    {
+        if (binary[i] == '1' || i == 31)
+        {
+            write(1, &binary[i], 1);
+            count++;
+        }
+    }
+
+    return count;
+}
+= NULL)
 		str = "(null)";
 
 	while (str[length] != '\0')
